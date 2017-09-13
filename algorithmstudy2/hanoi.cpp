03cf87e4b6dc5c63d023340ad17093f58cb7ec32
youngjae <juk1413@naver.com>
2017-09-13 20:03:36 +0900

    test

---
+++ algorithmstudy2/hanoi.cpp
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
+using namespace std;
+
+void hanoi(int n,int x,int y) {
+	if (n == 0)return;
+	hanoi(n - 1, x, 6 - x - y);
+	printf("%d %d\n",x,y);
+	hanoi(n - 1, 6 - x - y, y);
+}
+
+int main() {
+
+	int n;
+
+	scanf("%d",&n);
+	printf("%d\n",(1<<n)-1);
+
+	hanoi(n,1,3);
+
+	return 0;
+}
